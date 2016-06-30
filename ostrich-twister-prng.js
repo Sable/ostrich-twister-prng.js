e4b2714470dd67d2317b6adf0f85da456fd6a563
Erick Lavoie <github@ericklavoie.com>
2016-06-30 18:39:58 +0200

    Changed interface

--- ostrich-twister-prng.js
+++ ostrich-twister-prng.js
@@ -10074,6 +10074,6 @@ module.exports = Module;
 // Do not recurse into module and waste all day
 Module.inspect = function() { return '[Module]' }
 
-module.exports.genRand = module.exports._gen_rand;
+module.exports.random = module.exports._gen_rand;
 module.exports.seed = module.exports._init_genrand;
 
